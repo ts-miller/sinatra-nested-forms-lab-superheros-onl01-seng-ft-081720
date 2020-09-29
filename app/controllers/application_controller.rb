6ca02ed7869233ac78206d31c501a6410bbda3c0
ts-miller <tylerstephen11@gmail.com>
2020-09-29 17:54:33 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :form
+    end
 
+    post '/teams' do
+        @team = Team.new(params["team"])
+        params["team"]["heroes"].each do |hero|
+            Hero.new(hero)
+        end
+        @heroes = Hero.all
+        erb :team
+    end
 end
---
+++ app/models/team.rb
@@ -0,0 +1,8 @@
+class Team
+    attr_accessor :name, :motto
+
+    def initialize(params)
+        @name = params["name"]
+        @motto = params["motto"]
+    end
+end
